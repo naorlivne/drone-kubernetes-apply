f75725985c566a6d8f6f7e6e4c9dfb0a04882ebf
naorlivne <naorlivne@gmail.com>
2020-06-21 11:53:48 +0300

    escaping chars

--- drone_kubernetes_runner.sh
+++ drone_kubernetes_runner.sh
@@ -13,8 +13,8 @@ then
 K8S_API_HOST=" "
 else
 K8S_API_HOST="--server \"${PLUGIN_KUBERNETES_API_HOST}\""
-K8S_API_HOST="${K8S_API_HOST%\"}"
-K8S_API_HOST="${K8S_API_HOST#\"}"
+K8S_API_HOST="${K8S_API_HOST%\'}"
+K8S_API_HOST="${K8S_API_HOST#\'}"
 fi
 
 if [ -z "${PLUGIN_KUBERNETES_TOKEN}" ]
@@ -22,8 +22,8 @@ then
 K8S_TOKEN=" "
 else
 K8S_TOKEN="--token \"${PLUGIN_KUBERNETES_TOKEN}\""
-K8S_TOKEN="${K8S_TOKEN%\"}"
-K8S_TOKEN="${K8S_TOKEN#\"}"
+K8S_TOKEN="${K8S_TOKEN%\'}"
+K8S_TOKEN="${K8S_TOKEN#\'}"
 fi
 
 # run kubectl apply on the variable injected yaml config file
